544b993b7d507403b72831c59e1cb6633b8f2cc2
shusingh <ksingh.shubh@gmail.com>
2024-05-19 07:16:30 -0700

    fixed the home component overflowing issue

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import WelcomePage from '../WelcomePage/WelcomePage';
 
 const Home: React.FC = () => {
   return (
-    <div className="bg-white dark:bg-gray-800 flex flex-col mx-auto min-w-[640px] p-4">
+    <div className="bg-white dark:bg-gray-800 flex flex-col mx-auto p-4 max-w-screen-lg w-full">
       <WelcomePage />
     </div>
   );
--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,18 +1,21 @@
+import React from 'react';
 import SocialLinks from './components/SocialLinks';
 import WelcomeHeader from './components/WelcomeHeader';
 import WelcomeMessage from './components/WelcomeMessage';
 
 const WelcomePage: React.FC = () => {
   return (
-    <>
-      <div className="flex flex-col space-y-4">
-        <div className="flex justify-start items-center mb-4">
-          <WelcomeHeader />
-        </div>
+    <div className="flex flex-col items-start space-y-4 w-full">
+      <div className="flex justify-start items-center w-full">
+        <WelcomeHeader />
       </div>
-      <WelcomeMessage />
-      <SocialLinks />
-    </>
+      <div className="w-full">
+        <WelcomeMessage />
+      </div>
+      <div className="w-full">
+        <SocialLinks />
+      </div>
+    </div>
   );
 };
 
